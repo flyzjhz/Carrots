6ed34ae7281f53229b8932f196c19ccbc87d8efb
kyosold <kyosold@qq.com>
2016-03-11 10:35:34 +0800

    Update ctlog.h

--- ctlog/ctlog.h
+++ ctlog/ctlog.h
@@ -16,14 +16,14 @@ extern char log_sid[1024];
 #define	emerg	1
 
 
-#define log_emerg(fmt, ...) {if(log_level>=emerg){syslog(LOG_EMERG, "[EMERG] %s %s "fmt, __func__, msg_id, ##__VA_ARGS__);}}
-#define log_alert(fmt, ...) {if(log_level>=alert){syslog(LOG_ALERT, "[ALERT] %s %s "fmt, __func__, msg_id, ##__VA_ARGS__);}}
-#define log_crit(fmt, ...) {if(log_level>=crit){syslog(LOG_CRIT, "[CRIT] %s %s "fmt, __func__, msg_id, ##__VA_ARGS__);}}
-#define log_error(fmt, ...) {if(log_level>=error){syslog(LOG_ERR, "[ERROR] %s %s "fmt, __func__, msg_id, ##__VA_ARGS__);}}
-#define log_warning(fmt, ...) {if(log_level>=warning){syslog(LOG_WARNING, "[WARNING] %s %s "fmt, __func__, msg_id, ##__VA_ARGS__);}}
-#define log_notice(fmt, ...) {if(log_level>=notice){syslog(LOG_NOTICE, "[NOTICE] %s %s "fmt, __func__, msg_id, ##__VA_ARGS__);}}
-#define log_info(fmt, ...) {if(log_level>=info){syslog(LOG_INFO, "[INFO] %s %s "fmt, __func__, msg_id, ##__VA_ARGS__);}}
-#define log_debug(fmt, ...) {if(log_level>=debug){syslog(LOG_DEBUG, "[DEBUG] %s %s "fmt, __func__, msg_id, ##__VA_ARGS__);}}
+#define log_emerg(fmt, ...) {if(log_level>=emerg){syslog(LOG_EMERG, "[EMERG] %s %s "fmt, __func__, log_sid, ##__VA_ARGS__);}}
+#define log_alert(fmt, ...) {if(log_level>=alert){syslog(LOG_ALERT, "[ALERT] %s %s "fmt, __func__, log_sid, ##__VA_ARGS__);}}
+#define log_crit(fmt, ...) {if(log_level>=crit){syslog(LOG_CRIT, "[CRIT] %s %s "fmt, __func__, log_sid, ##__VA_ARGS__);}}
+#define log_error(fmt, ...) {if(log_level>=error){syslog(LOG_ERR, "[ERROR] %s %s "fmt, __func__, log_sid, ##__VA_ARGS__);}}
+#define log_warning(fmt, ...) {if(log_level>=warning){syslog(LOG_WARNING, "[WARNING] %s %s "fmt, __func__, log_sid, ##__VA_ARGS__);}}
+#define log_notice(fmt, ...) {if(log_level>=notice){syslog(LOG_NOTICE, "[NOTICE] %s %s "fmt, __func__, log_sid, ##__VA_ARGS__);}}
+#define log_info(fmt, ...) {if(log_level>=info){syslog(LOG_INFO, "[INFO] %s %s "fmt, __func__, log_sid, ##__VA_ARGS__);}}
+#define log_debug(fmt, ...) {if(log_level>=debug){syslog(LOG_DEBUG, "[DEBUG] %s %s "fmt, __func__, log_sid, ##__VA_ARGS__);}}
 
 void ctlog(const char *ident, int option, int facility);
 
